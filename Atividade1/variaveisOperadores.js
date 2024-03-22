30724beeb447dcf9b07a0f624fde75ea45e2ae70
OCTÁVIO LEITE NETO <octavio.neto@a.ucb.br>
2024-03-22 10:17:12 -0300

    update activity 1 and insert activity 2

--- Atividade1/variaveisOperadores.js
+++ Atividade1/variaveisOperadores.js
@@ -1,10 +1,10 @@
 let N1 = 100;
 let N2 = 35.5;
 let ope = N1 + N2;
-console.log("|Resultado da operação " + ope.toPrecision(5) +" entre " + N1 + " e " + N2)
+console.log(`Resultado da operação ${ope} entre ${N1} e ${N2}`)
 ope = N1 - N2;
-console.log("|Resultado da operação " + ope.toPrecision(5) +" entre " + N1 + " e " + N2)
+console.log(`Resultado da operação ${ope} entre ${N1} e ${N2}`)
 ope = N1 * N2;
-console.log("|Resultado da operação " + ope.toPrecision(5) +" entre " + N1 + " e " + N2)
+console.log(`Resultado da operação ${ope} entre ${N1} e ${N2}`)
 ope = N1 / N2;
-console.log("|Resultado da operação " + ope.toPrecision(5) +" entre " + N1 + " e " + N2)
+console.log(`Resultado da operação ${ope} entre ${N1} e ${N2}`)
